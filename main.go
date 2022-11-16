f2fae8101aae21e58215efa1002cc31441f28402
salihprnz07 <salihbaz07@gmail.com>
2022-11-16 15:43:58 +0300

    command line

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import "golesson/variables"
-
+	//"fmt"
+	/*
+	birden fazla package eklenirken () arasına yazılı, paketler arasına birşey konulmaz!!
+	*/
+	
 func main() {
 	variables.Demo1()
 }
