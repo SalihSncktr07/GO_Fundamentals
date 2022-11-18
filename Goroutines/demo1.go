faad2372fc9d59a7ca67a7d12fcfee95405b5535
salihprnz07 <salihbaz07@gmail.com>
2022-11-18 22:04:08 +0300

    gorutines fix

--- Goroutines/demo1.go
+++ Goroutines/demo1.go
@@ -13,3 +13,10 @@ func TekSayi() {
 		fmt.Println("Tek sayı:", i)
 	}
 }
+
+/*
+go goroutines.CiftSayi()
+	go goroutines.TekSayi()
+	time.Sleep(5 * time.Second) //5 sn (eğer 5 yazılmasaydı default 1sn olurdu)
+	fmt.Println("Main bitti")
+*/
