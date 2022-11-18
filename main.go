6d14968b2d6a09702251c94c1234cd4ab00daf45
salihprnz07 <salihbaz07@gmail.com>
2022-11-18 21:34:41 +0300

    Goroutines demo1

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"golesson/structs"
+	"fmt"
+	"golesson/goroutines"
+	"time"
 )
 
 func main() {
-	structs.Demo2()
+	go goroutines.CiftSayi()
+	go goroutines.TekSayi()
+	time.Sleep(5 * time.Second) //5 sn (eğer 5 yazılmasaydı default 1sn olurdu)
+	fmt.Println("Main bitti")
 }
