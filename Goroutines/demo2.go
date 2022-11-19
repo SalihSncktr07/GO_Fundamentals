bd6c558be83cb8d53b0882a18c2d6b0bee1150cf
salihprnz07 <salihbaz07@gmail.com>
2022-11-19 20:38:32 +0300

    fix

--- Goroutines/demo2.go
+++ Goroutines/demo2.go
@@ -24,3 +24,15 @@ func TekSayilar(tekCn chan int) {
 
 	tekCn <- toplam
 }
+
+/*
+	tekCn := make(chan int)
+	ciftCn := make(chan int)
+	go channels.TekSayilar(tekCn)
+	go channels.CiftSayilar(ciftCn)
+
+	ciftSayiToplam, tekSayiToplam := <-ciftCn, <-tekCn
+
+	carpim := ciftSayiToplam * tekSayiToplam
+	fmt.Println("Çarpım:", carpim)
+*/
