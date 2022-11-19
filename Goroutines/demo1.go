204cba6faffe7b7737011b8e23bb53706d10ffa8
salihprnz07 <salihbaz07@gmail.com>
2022-11-19 20:23:52 +0300

    goRoutined demo1 fix

--- Goroutines/demo1.go
+++ Goroutines/demo1.go
@@ -1,22 +1,27 @@
 package goroutines
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func CiftSayi() {
 	for i := 0; i <= 10; i += 2 {
 		fmt.Println("Çift sayı:", i)
+		time.Sleep(time.Second)
 	}
 }
 
 func TekSayi() {
 	for i := 1; i <= 10; i += 2 {
 		fmt.Println("Tek sayı:", i)
+		time.Sleep(time.Second)
 	}
 }
 
 /*
-go goroutines.CiftSayi()
 	go goroutines.TekSayi()
+	go goroutines.CiftSayi()
 	time.Sleep(5 * time.Second) //5 sn (eğer 5 yazılmasaydı default 1sn olurdu)
 	fmt.Println("Main bitti")
 */
--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	go goroutines.CiftSayi()
 	go goroutines.TekSayi()
+	go goroutines.CiftSayi()
 	time.Sleep(5 * time.Second) //5 sn (eğer 5 yazılmasaydı default 1sn olurdu)
 	fmt.Println("Main bitti")
 }
