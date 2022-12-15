8d3a0e6057d11fe0f549f125bc501e8acfa7fdf2
Mr.Salih <71170042+salihprnz07@users.noreply.github.com>
2022-12-15 14:53:38 +0300

    çalışma sırası

--- defer_Statment/demo1.go
+++ defer_Statment/demo1.go
@@ -2,16 +2,16 @@ package defer_statment
 
 import "fmt"
 
-func A() {
+func A() { // A
 	fmt.Println("a fonk çalıştı")
 }
 
-func B() {
+func B() { // B C A
 	defer A()
 	defer C()
 	fmt.Println("b fonk çalıştı")
 }
 
-func C() {
+func C() { // C
 	fmt.Println("c fonk çalıştı")
 }
